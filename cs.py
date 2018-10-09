8d1281929be8c4bf090f58be52ba7fc2c86b1bf7
mpc97 <manuel.coimbra@outlook.pt>
2018-10-09 18:53:06 +0100

    Update cs.py
    Resolvi o bloqueio que isto tinha já está a funcionar fixe

--- cs.py
+++ cs.py
@@ -18,12 +18,20 @@ HOME = os.getcwd()
 cs_address = (HOST, CSPORT)
 
 def utf8len(string):
+	'''utf8len : string -> int
+	:: recebe um argumento do tipo string e devolve o tamanho
+	dessa string em bytes'''
 	return len(string.encode('utf-8'))
 
 def empty_dir(path):
+	'''empty_dir : string -> logico
+	:: recebe um argumento do tipo string que representa o path para uma
+	diretoria, devolve True caso ela esteja vazia e False caso contrario'''	
 	return len(os.listdir(path)) == 0
 
 def tcp_init():
+	'''tcp_init : {} -> connection
+	:: inicia uma ligacao tcp (servidor) e devolve uma connection'''
 	s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	s_tcp.bind(cs_address)
@@ -32,24 +40,35 @@ def tcp_init():
 	return connection_tcp
 
 def udp_server_init():
+	'''udp_server_init : {} -> connection
+	:: inicia uma ligacao udp (servidor) e devolve uma connection'''
 	c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	c.bind(cs_address)
 	return c
 
 def udp_client_init(address):
+	'''udp_client_init : {} -> connection
+	:: recebe um argumento do tipo address, inicia uma ligacao udp (cliente)
+	e devolve uma connection'''
 	c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	c.bind(address)
 	return c
 
 def tcp_receive(connection):
+	'''tcp_receive : connection -> string
+	:: recebe um argumento do tipo connection e recebe uma mensagem atraves
+	de uma ligacao tcp e devolve-a'''
 	msg = connection.recv(BUFFER_SIZE)
-	if msg == 0:
+	if msg == "":
 		return None
-	while msg[-1] != "\n":
+	while msg[-1:] != "\n":
 		msg += connection.recv(BUFFER_SIZE)
 	return msg
 
 def udp_receive(connection):
+	'''udp_receive : connection -> string
+	:: recebe um argumento do tipo connection e recebe uma mensagem atraves
+	de uma ligacao udp e devolve-a'''
 	msg = connection.recvfrom(BUFFER_SIZE)
 	if msg == 0:
 		return None
@@ -58,6 +77,8 @@ def udp_receive(connection):
 	return msg
 
 def udp_send(connection, msg, address):
+	'''udp_send : connection x string x address -> {}
+	:: envia uma mensagem atraves de uma ligacao udp'''
 	total = utf8len(msg)
 	sent = 0
 	while sent < total:
@@ -75,14 +96,46 @@ def udp_terminate(connection):
 def tcp_terminate(connection):
 	connection.close()
 
-def get_bs_address(d):
-	path = HOME + "/" + dirname + "/" + d
+def get_bs_address(user, d):
+	path = HOME + "/user_" + user + "/" + d
 	os.chdir(path)
 	f = open("ip_port.txt", "r")
 	line = f.read()
 	line_info = line.split()
 	os.chdir(HOME)
 	return line_info
+
+def get_available_bs():
+	f = open("bs_list.txt", "r")
+	bs = f.readline()
+	f.close()
+	if bs != "":
+		bs_info = bs.split()
+		ip_bs = bs_info[0]
+		port_bs = bs_info[1]
+		return (ip_bs, port_bs)
+	else:
+		return None
+
+def registered_in_bs(user, bs):
+	path = HOME + "/" + "user_" + user
+	os.chdir(path)
+	f = open("bss.txt", "r")
+	bss = f.readlines()
+	for l in bss:
+		if l == bs:
+			os.chdir(HOME)
+			return True
+	os.chdir(HOME)
+	return False
+
+def register_in_bs(user, bs):
+	path = HOME + "/" + "user_" + user
+	os.chdir(path)
+	f = open("bss.txt", "a")
+	f.write(bs)
+	f.close()
+	os.chdir(HOME)
 
 def handle_bs(connection, msg):
 	data_list = msg.split()
@@ -101,7 +154,7 @@ def handle_bs(connection, msg):
 			return
 		bs = ip_bs + " " + port_bs + "\n"
 		try:
-			f = open("bs_list", "w")
+			f = open("bs_list.txt", "a")
 			f.write(bs)
 			f.close()
 		except IOError as e:
@@ -124,7 +177,7 @@ def handle_bs(connection, msg):
 			return
 		bs = ip_bs + " " + port_bs + "\n"
 		try:
-			f = open("bs_list", "r")
+			f = open("bs_list.txt", "r")
 			bs_list = f.readlines()
 			f.close()
 			f = open("bs_list", "w")
@@ -154,13 +207,11 @@ def handle_user(connection, aut):
 		if os.path.exists(filename):
 			f = open(filename, "r")
 			data = f.read()
+			f.close()
 			if data == password:
-				f.close()
 				print "User: %s" % user
 				reply += "OK\n"
-				tcp_send(connection, reply)
 			else:
-				f.close()
 				print "User entered wrong password: %s" % user
 				reply += "NOK\n"
 				tcp_send(connection, reply)
@@ -174,6 +225,7 @@ def handle_user(connection, aut):
 			reply += "NEW\n"
 			tcp_send(connection, reply)
 			return
+		tcp_send(connection, reply)
 
 	msg = tcp_receive(connection)
 
@@ -198,16 +250,61 @@ def handle_user(connection, aut):
 
 		elif command == "BCK": #incompleto
 			reply += "BKR "
+			bs_address = get_available_bs()
+			if bs_address == None:
+				print "No BS available"
+				reply += " EOF\n"
+				tcp_send(connection, reply)
+			else:
+				user_dir = data_list[1]
+				n = data_list[2]
+				files = data_list[3:]
+				ip_bs = bs_address[0]
+				port_bs = bs_address[1]
+				reply += ip_bs + " " + port_bs
+				if registered_in_bs(user, str(bs_address)):
+					msg = "LSF " + user + " " + user_dir + "\n"
+					c = udp_client_init(bs_address)
+					udp_send(c, msg, bs_address)
+					bs_msg = udp_receive(c)
+					udp_terminate(c)
+					bs_msg_list = bs_msg.split()
+					response = bs_msg_list[0]
+					n = bs_msg_list[1]
+					bs_files = bs_msg_list[2:]
+					if response == "LFD":
+						reply += " " + n
+						for i in bs_files:
+							reply += " " + i
+						reply += "\n"
+					tcp_send(connection, reply)
+				else:
+					register_in_bs(user, str(bs_address))
+					msg = "LSU " + user + " " + password + "\n"
+					c = udp_client_init(bs_address)
+					udp_send(c, msg, bs_address)
+					bs_msg = udp_receive(c)
+					udp_terminate(c)
+					bs_msg_list = bs_msg.split()
+					response = bs_msg_list[0]
+					status = bs_msg_list[1]
+					if response == "LUR":
+						if status == "OK":
+							reply += "OK\n"
+						elif status == "NOK":
+							reply += "NOK\n"
+						elif status == "ERR":
+							reply += "ERR\n"
+					tcp_send(connection, reply)
 
-
-		elif command == "RST": #incompleto
+		elif command == "RST": #completo
 			reply += "RSR "
 			rst_dir = data_list[1]
-			path = HOME + "/" + dirname + "/" + rst_dir
-			if os.path.exists(path):
-				os.chdir(path)
-				f = open("ip_port.txt", "r")
-				ip_port = f.read()
+			bs_info = get_bs_address(user, rst_dir)
+			ip_bs = bs_info[0]
+			port_bs = bs_info[1]
+			reply += ip_bs + " " + port_bs
+			tcp_send(connection, reply)
 
 		elif command == "LSD": #completo
 			print "List request"
@@ -226,7 +323,7 @@ def handle_user(connection, aut):
 			user_dir = data_list[1]
 			msg = "DLB "
 			user_dir = data_list[1]
-			bs_info = get_bs_address(user_dir)
+			bs_info = get_bs_address(user, user_dir)
 			ip_bs = bs_info[0]
 			port_bs = bs_info[1]
 			bs_address = (ip_bs, port_bs)
@@ -250,12 +347,12 @@ def handle_user(connection, aut):
 			reply += "DDR "
 			msg = "DLB "
 			user_dir = data_list[1]
-			bs_info = get_bs_address(user_dir)
+			bs_info = get_bs_address(user, user_dir)
 			ip_bs = bs_info[0]
 			port_bs = bs_info[1]
 			bs_address = (ip_bs, port_bs)
 			c = udp_client_init(bs_address)
-			msg += user + " " + user_dir
+			msg += user + " " + user_dir + "\n"
 			udp_send(c, msg, bs_address)
 			bs_msg = udp_receive(c)
 			udp_terminate(c)
@@ -275,10 +372,15 @@ def handle_user(connection, aut):
 
 def user_tcp():
 	while True:
+		print "Start"
 		c = tcp_init()
+		print "Init check"
 		msg = tcp_receive(c)
+		print "Receive check"
 		handle_user(c, msg)
-		tcp_terminate()
+		print "Handle check"
+		tcp_terminate(c)
+		print "Terminate check"
 	
 
 def bs_udp():
@@ -288,6 +390,10 @@ def bs_udp():
 		handle_bs(c, msg)
 
 def main():
+	print HOST
+	print "\n"
+	print CSPORT
+	print "\n"
 	try:
 		pid = os.fork()
 	except OSError as e:
