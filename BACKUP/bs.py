ae1b0e9383721bf0271ed0e0d00a18707bba3d3d
pedroggbcampos <38165332+pedroggbcampos@users.noreply.github.com>
2018-10-12 17:37:39 +0100

    Add files via upload

---
+++ BACKUP/bs.py
@@ -0,0 +1,475 @@
+import sys
+import socket
+import argparse
+import string
+import os
+import shutil
+import signal
+from threading import Thread
+
+BUFFER_SIZE=80
+
+parser= argparse.ArgumentParser(description='Process invoking command.')
+parser.add_argument('-b', '--BSport', default=59000, type=int, required=False, help='port where the BS server accepts requests')
+parser.add_argument('-n', '--CSname', default=socket.gethostname(), type=str, required=False, help='name of the machine where the CS runs')
+parser.add_argument('-p', '--CSport', default=58023, type=int, required=False, help='port where the CS server accepts requests')
+
+args = parser.parse_args()
+
+BSNAME = socket.gethostname()
+BSPORT= int(args.BSport)
+CSNAME= str(args.CSname)
+CSPORT= int(args.CSport)
+
+#BSPORT = socket.getsockname()[1]
+IPBS = socket.gethostbyname(BSNAME)
+
+udp_connection = None
+bs_addr = (IPBS, BSPORT)
+
+recent_user = ""
+HOME = os.getcwd()
+
+def tcp_thread():
+	while True:
+		tcp_server()
+	return
+
+def tcp_server():
+	tcp_addr=("", BSPORT)
+
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except socket.error:
+		print 'Error creating socket [TCP]'
+		sys.exit()
+
+	try:
+		s.bind(tcp_addr)
+	except socket.error:
+		print 'Error binding socket to address[TCP]'
+		sys.exit()
+
+	try:
+		s.listen(1)
+	except socket.error:
+		print 'Error enabling server[TCP]'
+
+	try:
+		connection, client_address = s.accept()
+	except socket.error:
+		print 'Error accepting connection'
+	while (True):
+		try:
+			data = connection.recv(BUFFER_SIZE)
+			if data == "":
+				break
+			elif data == 0:
+				print 'Error transmiting data[TCP]'
+				break
+			while(data[-1]!="\n"):
+				data += connection.recv(BUFFER_SIZE)
+			print data
+			msg = handler_USER(data)
+			connection.send(msg)
+			break
+		except socket.error:
+			print 'Error transmiting data[TCP]'
+			break
+	while (True):
+		try:
+			data = connection.recv(BUFFER_SIZE)
+			if data == "":
+				break
+			elif data == 0:
+				print 'Error transmiting data[TCP]'
+				break
+			while(data[-1]!="\n"):
+				data += connection.recv(BUFFER_SIZE)
+			print data
+			msg = handler_USER(data)
+			connection.send(msg)
+			break
+		except socket.error:
+			print 'Error transmiting data[TCP]'
+			break
+
+	connection.close()
+
+	return
+
+def udp_thread():
+	s = udp_server_init()
+	global udp_connection
+	udp_connection = s
+	udp_server(s)
+	#while(True):
+		#udp_server(s)
+	#return
+
+def udp_server_register():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    ip_cs = socket.gethostbyname(CSNAME)
+    cs_addr =(ip_cs, CSPORT)
+    
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #s.bind(cs_addr)
+    
+    msg = "REG " + str(IPBS) + " " + str(BSPORT) + "\n"
+    
+    bytes_sent = s.sendto(msg, cs_addr)
+    
+    data, cs_addr = s.recvfrom(BUFFER_SIZE)
+    handler_CS(data)
+    s.close()
+    #return
+    
+'''ip_cs = socket.gethostbyname(CSNAME)
+
+	cs_addr =(ip_cs, CSPORT)
+
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	except socket.error:
+		print 'Error creating socket [UDP]'
+		sys.exit()
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+	msg = "REG " + str(IPBS) + " " + str(BSPORT) + "\n"
+	n_bytes = len(msg)
+	try:
+		bytes_sent = s.sendto(msg, cs_addr)
+		print "mandei para : " + str(cs_addr)
+		print(msg)
+		if bytes_sent != n_bytes or bytes_sent == -1:
+			print 'Error sending data[UDP]'
+	except socket.error:
+		print 'Error sending data[UDP]'
+
+	data, cs_addr = s.recvfrom(BUFFER_SIZE)
+	print (data)
+	handler_CS(data)
+
+	s.close()'''
+
+
+def udp_server_init():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(bs_addr)
+    print bs_addr == ('192.168.1.2', 59000)
+    return s
+'''
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	except socket.error:
+		print 'Error creating socket [UDP]'
+		sys.exit()
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	try:
+		s.bind(bs_addr)
+	except socket.error:
+		print 'Error binding socket to address[UDP]'
+		sys.exit()
+
+	print bs_addr
+	return s'''
+
+def udp_server(s):
+    print 'vasco vai receber no endereco: ' + str(bs_addr)
+    data, addr = s.recvfrom(BUFFER_SIZE)
+    print 'pais'
+    msg = handler_CS(data)
+    bytes_sent = s.sendto(msg, addr)
+    #return
+    
+''''
+	ip_cs = socket.gethostbyname(CSNAME)
+
+	cs_addr =(ip_cs, CSPORT)
+
+	try:
+		print 'vasco vai receber no endereco: ' + str(bs_addr)
+		data, addr = s.recvfrom(BUFFER_SIZE)
+		print 'pais'
+		#if data==0:
+			#return None
+		#while data[-1:] != "\n":
+			#data+= s.recvfrom(BUFFER_SIZE)
+		print (data)
+		msg = handler_CS(data)
+		bytes = len(msg)
+		try:
+			bytes_sent = s.sendto(msg, addr)
+			if bytes_sent != bytes or bytes_sent == -1:
+				print 'Error sending data[UDP]'
+		except socket.error:
+			print 'Error sending data[UDP]'
+	except socket.error:
+		print 'Error transmiting data[UDP]'
+
+	return'''
+
+def udp_unregister():
+
+	ip_cs = socket.gethostbyname(CSNAME)
+
+	cs_addr =(ip_cs, CSPORT)
+
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	except socket.error:
+		print 'Error creating socket [UDP]'
+		sys.exit()
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	try:
+		s.bind(bs_addr)
+	except socket.error:
+		print 'Error binding socket to address[UDP]'
+		sys.exit()
+
+	msg = "UNR " + IPBS + " " + str(BSPORT)
+	bytes = len(msg)
+	try:
+		bytes_sent = s.sendto(msg, cs_addr)
+		if bytes_sent != bytes or bytes_sent == -1:
+			print 'Error sending data[UDP]'
+	except socket.error:
+		print 'Error sending data[UDP]'
+	try:
+		data, addr=s.recvfrom(BUFFER_SIZE)
+		if data == 0:
+			return None
+		while data[-1] != "\n":
+			data+= s.recvfrom(BUFFER_SIZE)
+		msg = data.split(" ")
+		if msg[1] == "UAR":
+			if msg[2] == "OK\n":
+				s.close()
+				return "OK"
+			elif msg[2] == "NOK\n":
+				s.close()
+				return "NOK" 
+			else:
+				s.close()
+				return "ERR"
+		else:
+			s.close()
+			return "ERR"
+	except socket.error:
+		s.close()
+		return "ERR"
+
+	s.close()
+	return "ERR"
+
+
+def handler_CS(msg):
+	path="/BS/user"
+
+	msg = msg.split(" ")
+	n_msg = "ERR"
+
+	if msg[0]=="RGR":
+		if (msg[1]=="NOK\n" or msg[1]=="ERR\n"):
+			os._exit(0)
+		elif(msg[1] == "OK\n"):
+			return "LOL"
+
+	elif msg[0]=="UAR":
+		if (msg[1]=="NOK\n" or msg[1]=="ERR\n"):
+			os._exit(0)
+
+	elif msg[0]=="LSF":
+		if (len(msg)==3 and len(msg[1])==5):
+			if find_dir(msg[1],msg[2]):
+				n_msg = "LFD "
+				dir_path = "/user_" + msg[1] + "/" + msg[2]
+				files = os.listdir(dir_path)
+				n_msg = n_msg + len(files) + " "
+				for file in files:
+					mtime = os.path.getmtime(dir_path + "/" + file)
+					last_modified_date = datetime.datetime.fromtimestamp(mtime)
+					file_size = os.path.getsize(dir_path + "/" + file)
+					last_modified_date = str(last_modified_date)
+					last_modified_date = last_modified_date.split(".")
+					last_modified_date = last_modified_date[0].decode('utf-8').replace("-".decode('utf-8'), ".").encode('utf-8')
+					n_msg = n_msg + file + " " + last_modified_date + " " + str(file_size) + " "
+				n_msg += "\n"
+			else:
+				n_msg = "ERR\n"
+		else:
+			n_msg = "ERR\n"
+
+	elif msg[0]=="LSU":
+		try:
+			if (len(msg)==3):
+				if is_user(msg[1], msg[2]):
+					n_msg = "LUR NOK\n"
+				else:
+					add_user_bs(msg[1], msg[2])
+					n_msg = "LUR OK\n"
+					print ("New user: %s" % msg[1])
+			else:
+				n_msg="LUR ERR\n"
+		except ValueError:
+			n_msg="LUR ERR\n"
+
+	elif msg[0]=="DLB":
+		if (len(msg)==3 and isinstance(int(msg[1]), int) and len(msg[1])==5):
+			if find_dir(msg[1],msg[2]):
+				remove_dir(msg[1], msg[2])
+				n_msg = "DBR OK\n"
+			else:
+				n_msg = "DBR NOK\n"
+		else:
+			n_msg = "DBR ERR\n"
+
+	else:
+		n_msg="ERR\n"
+
+	return n_msg
+
+def remove_dir(nid, dir):
+	path="/user_" + nid + "/" + dir
+	shutil.rmtree(path)
+	return
+
+def is_user(id, p):
+	try:
+		f = open("user_" + id + ".txt", "r")
+		if p in f.read():
+			f.close()
+			return True
+		else:
+			f.close()
+			return False
+	except IOError:
+		return False
+
+
+def add_user_bs(id, p):
+	path="user_"+ id
+	file=open(path + ".txt", "w")
+	file.write(p)
+	file.close()
+	if not os.path.exists(path):
+		os.mkdir(path)
+	return
+
+def handler_USER(msg):
+	path="/BS/user"
+	mens = msg.split(" ")
+
+	if mens[0]=="AUT":
+		if is_user(mens[1], mens[2]):
+			print "User: %s", mens[1]
+			n_msg="AUR OK\n"
+			global recent_user
+			recent_user = mens[1]
+		else:
+			n_msg="AUR NOK\n"
+
+	elif mens[0]=="UPL":
+		print(recent_user)
+		data = msg
+		bite = 3 + 3 + len(mens[1]) + len(mens[2]) 
+		nr_files = mens[2]
+		direc = mens[1]
+		data = data[bite:]
+		file_names = ""
+		os.chdir("user_" + recent_user)
+		os.mkdir(mens[1])
+		print(os.getcwd())
+		directory = mens[1]
+		os.chdir(directory)
+		print(os.getcwd())
+		for f in range(0, int(nr_files)):
+			file_info = data.split(" ")
+			bite = len(file_info[0]) + len(file_info[1]) + len(file_info[2]) + 3
+			data = data[bite:]
+			print file_info
+
+			file = open(file_info[0] , 'wb')
+			for c in range (0, int(file_info[2])):
+				file.write(data[c])
+			file.close()
+			file_names += file_info[0] + "\n"
+			bite = int(file_info[2]) + 1
+			data = data[bite:]
+		os.chdir(HOME)
+		print("Successful restore")
+		print("Directory: %s" % direc)
+		print(file_names)
+
+		
+	elif mens[0]=="RSB":
+		if (len(mens)==2 and isinstance(mens[1], str)):
+			if find_dir(mens[1]):
+				n_msg = "RBR "
+				path = "/" + mens[1] 
+				files = os.listdir(dir_path)
+				n_msg = n_msg + len(files) + " "
+				for file in files:
+					mtime = os.path.getmtime(dir_path + "/" + file)
+					last_modified_date = datetime.datetime.fromtimestamp(mtime)
+					file_size = os.path.getsize(dir_path + "/" + file)
+					last_modified_date = str(last_modified_date)
+					last_modified_date = last_modified_date.split(".")
+					last_modified_date = last_modified_date[0].decode('utf-8').replace("-".decode('utf-8'), ".").encode('utf-8')
+					n_msg = n_msg + file + " " + last_modified_date + " " + str(file_size) + " "
+
+					file = open(path, mode="r")
+					content = read_file(path)
+					n_msg += content
+
+				n_msg += "\n"
+			else:
+				n_msg="RBR EOF\n"
+		else:
+			n_msg="RBR ERR\n"
+
+	else:
+		n_msg="ERR\n"
+
+	return n_msg
+
+
+def read_file(file_path):
+	file = open(file_path, mode="r")
+	content = file.read()
+	file.close()
+	return content
+
+def transfer(dir, n, files):
+	return
+
+def find_dir(user, dir):
+	return os.path.isdir("/user_" + user + "/" + dir)
+
+def signal_handler(sig, frame):
+	msg = udp_unregister()
+	if msg == "OK":
+		print("\n")
+		sys.exit(0)
+	elif msg =="NOK":
+		signal.signal(signal.SIGINT, signal_handler)
+		return
+
+
+def main():
+	udp_server_register() 
+	t1 = Thread(target=udp_thread)
+	t2 = Thread(target=tcp_thread)
+	t1.daemon = True
+	t2.daemon = True
+	t1.start()
+	t2.start()
+	signal.signal(signal.SIGINT, signal_handler)
+	signal.pause()
+
+
+if __name__ =='__main__':
+	main()
---
+++ CENTRAL/cs.py
@@ -0,0 +1,543 @@
+#!/usr/bin/env python
+import socket
+import sys
+import argparse
+import string
+import os
+import multiprocessing
+
+#Faltam os timeouts
+
+parser = argparse.ArgumentParser(description='Process invoking command.')
+parser.add_argument('-p', '--CSport', default=58023, type=int, required=False, help='port where the CS server accepts requests')
+
+args = parser.parse_args()
+
+HOST = socket.gethostbyname(socket.gethostname())
+BUFFER_SIZE = 80
+BACKLOG = 1
+CSPORT = int(args.CSport)
+HOME = os.getcwd()
+
+cs_address = (HOST, CSPORT)
+
+lock = multiprocessing.Lock()
+
+def empty_dir(path):
+	'''empty_dir : string -> logico
+	:: recebe um argumento do tipo string que representa o path para uma
+	diretoria, devolve True caso ela esteja vazia e False caso contrario'''	
+	return len(os.listdir(path)) == 0
+
+def tcp_init():
+	'''tcp_init : {} -> connection
+	:: inicia uma ligacao tcp (servidor) e devolve uma connection'''
+	try:
+		s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s_tcp.bind(cs_address)
+		s_tcp.listen(BACKLOG)
+		connection_tcp, client_address = s_tcp.accept()
+	except socket.error as e:
+		print("Error initiating tcp connection: %s" % e)
+	return connection_tcp
+
+def udp_server_init():
+	'''udp_server_init : {} -> connection
+	:: inicia uma ligacao udp (servidor) e devolve uma connection'''
+	try:
+		c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		c.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		c.bind(cs_address)
+	except socket.error as e:
+		print("Error initiating udp server connection: %s" % e)
+	return c
+
+def udp_client_init():
+	'''udp_client_init : {} -> connection
+	:: inicia uma ligacao udp (cliente) e devolve uma connection'''
+	try:
+		c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		c.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	
+	except socket.error as e:
+		print("Error initiating udp client connection: %s" % e)
+	return c
+
+def tcp_receive(connection):
+	'''tcp_receive : connection -> string
+	:: recebe um argumento do tipo connection e recebe uma mensagem atraves
+	de uma ligacao tcp e devolve-a'''
+	try:
+		msg = connection.recv(BUFFER_SIZE)
+		if msg == "":
+			return None
+		while msg[-1:] != "\n":
+			msg += connection.recv(BUFFER_SIZE)
+	except socket.error as e:
+		print("Error receiving message from tcp connection: %s" % e)
+	return msg
+
+def udp_receive(connection):
+	'''udp_receive : connection -> string
+	:: recebe um argumento do tipo connection e recebe uma mensagem atraves
+	de uma ligacao udp e devolve-a'''
+	try:
+		msg, bs_address = connection.recvfrom(BUFFER_SIZE)
+	except socket.error as e:
+		print("Error receiving message from udp connection: %s" % e)
+	return (msg, bs_address)
+
+def udp_send(connection, msg, address):
+	'''udp_send : connection x string x address -> {}
+	:: envia uma mensagem atraves de uma ligacao udp'''
+	total = len(msg.encode())
+	try:
+		sent = connection.sendto(msg, address)
+		print address == ('192.168.1.2', 59000)
+	except socket.error as e:
+		print("Error sending message through udp connection: %s" % e)
+	#if sent != total :
+		#tratar erro udp
+
+def tcp_send(connection, msg):
+	'''tcp_send : connection x string -> {}
+	:: recebe um argumento do tipo connection e uma string que representa a mensagem
+	a enviar e envia a mensagem atraves de uma ligacao tcp'''
+	total = len(msg.encode())
+	sent = 0
+	while sent < total:
+		try:
+			sent += connection.send(msg)
+		except socket.error as e:
+			print("Error sending message through tcp connection: %s" % e)
+
+def udp_terminate(connection):
+	'''udp_terminate : connection -> {}
+	:: recebe um argumento do tipo connection e fecha-a'''
+	try:
+		connection.close()
+	except socket.error as e:
+		print("Error closing udp connection: %s" % e)
+
+def tcp_terminate(connection):
+	'''tcp_terminate : connection -> {}
+	:: recebe um argumento do tipo connection e fecha-a'''
+	try:
+		connection.close()
+	except socket.error as e:
+		print("Error closing tcp connection: %s" % e)
+
+def get_bs_address(user, d):
+	'''get_bs_address : string x string -> list
+	:: recebe um argumento do tipo string que representa um utilizador, uma string que
+	representa uma diretoria e devolve uma lista com o ip e o porto do servidor BS que
+	guardou essa diretoria'''
+	path = HOME + "/user_" + user + "/" + d
+	if not os.path.exists(path):
+		return None
+	try:
+		os.chdir(path)
+		f = open("ip_port.txt", "r")
+		line = f.read()
+		line_info = line.split()
+		os.chdir(HOME)
+	except IOError as e:
+		print("Error getting BS address: %s" % e)
+	return line_info
+
+def get_available_bs():
+	'''get_available_bs : {} -> list
+	:: consulta a lista de BS's registados caso haja um disponivel devolve uma lista com
+	o seu ip e porto, caso contrario devolve None'''
+	try:
+		f = open("bs_list.txt", "r")
+		bs = f.readline()
+		f.close()
+	except IOError as e:
+		print("Error getting available BS: %s" % e)
+	if bs != "":
+		bs_info = bs.split()
+		return bs_info
+	else:
+		return None
+
+def register_in_bs(user, bs):
+	'''register_in_bs : string x string -> {}
+	:: recebe um argumento do tipo string que representa um utilizador e uma string que
+	representa um servidor BS e regista esse BS no ficheiro de servidores BS onde esse
+	utilizador esta registado'''
+	filename = "bss_" + user + ".txt"
+	try:
+		f = open(filename, "a")
+		f.write(bs)
+		f.close()
+	except IOError as e:
+		print("Error registering in BS: %s" % e)
+
+def registered_in_bs(user, bs):
+	'''registered_in_bs : string x string -> logico
+	:: recebe um argumento do tipo string que representa um utilizador e uma string que
+	representa um servidor BS e devolve True caso esse utilizador ja se tenha registado
+	nesse servidor BS'''
+	filename = "bss_" + user + ".txt"
+	try:
+		f = open(filename, "r")
+		bss = f.readlines()
+		f.close()
+	except IOError as e:
+		print("Error checking for regist in BS: %s" % e)
+	for l in bss:
+		if l == bs:
+			return True
+	return False
+
+def check_protocol(command, args):
+	'''check_protocol : string x list -> logico
+	:: recebe um argumento do tipo string que representa o tipo de mensagem recebida e uma
+	lista com os restantes argumentos da mensagem, devolve True caso a mensagem esteja de
+	acordo com o protocolo e False caso contrario'''
+	n_args = len(args)
+	#print "vou verificar uma mensagem com len " + str(n_args)
+	if command == "AUT":
+		return n_args == 3
+	elif command == "DLU":
+		return n_args == 1
+	elif command == "BCK":
+		n = int(args[2])
+		return n_args == (3+n*4)
+	elif command == "RST":
+		return n_args == 2
+	elif command == "LSD":
+		return n_args == 1
+	elif command == "LSF":
+		return n_args == 2
+	elif command == "DEL":
+		return n_args == 2
+	elif command == "REG":
+		return n_args == 3
+	elif command == "UNR":
+		return n_args == 3
+	elif command == "LFD":
+		n = int(args[1])
+		return n_args == (2+n*4)
+	elif command == "LUR":
+		return n_args == 2
+	elif command == "DBR":
+		return n_args == 2
+	return False
+
+def handle_bs(connection, msg, bs_address):
+	reply = ""
+	data_list = msg.split()
+	command = data_list[0]
+	ip_bs = data_list[1]
+	port_bs = data_list[2]
+	bs = ip_bs + " " + port_bs + "\n"
+
+	if command == "REG": #completo
+		reply += "RGR "
+		if not check_protocol(command, data_list):
+			print "Protocol (syntax) error"
+			reply += "ERR\n"
+		else:
+			try:
+				f = open("bs_list.txt", "a")
+				f.write(bs)
+				f.close()
+			except IOError as e:
+				print ("Error registering BS: %s" % e)
+				reply += "NOK\n"
+
+			print "+BS: " + ip_bs + " " + port_bs
+			reply += "OK\n"
+
+	elif command == "UNR": #incompleto falta saber se o bs pode mesmo ir embora
+		reply += "UAR "
+		if not check_protocol(command, data_list):
+			print "Protocol (syntax) error"
+			reply += "ERR\n"
+		else:
+			try:
+				f = open("bs_list.txt", "r")
+				bs_list = f.readlines()
+				f.close()
+				os.remove("bs_list.txt")
+				f = open("bs_list.txt", "w")
+				for line in bs_list:
+					if line != bs:
+						f.write(line)
+				f.close()
+			except IOError as e:
+				print ("Error deregistering BS: %s" % e)
+				reply += "NOK\n"
+			print "-BS: " + ip_bs + " " + port_bs
+			reply += "OK\n"
+	udp_send(connection, reply, bs_address)
+
+def handle_user(connection, aut, lock):
+	aut_list = aut.split()
+	aut_command = aut_list[0]
+	reply = ""
+
+	if aut_command == "AUT": #completo
+		reply += "AUR "
+		if not check_protocol(aut_command, aut_list):
+			print "Protocol (syntax) error"
+			reply += "ERR\n"
+		else:
+			user = aut_list[1]
+			password = aut_list[2]
+			dirname = "user_" + user
+			filename = dirname + ".txt"
+			if os.path.exists(filename):
+				f = open(filename, "r")
+				data = f.read()
+				f.close()
+				if data == password:
+					print "User: %s" % user
+					reply += "OK\n"
+				else:
+					print "User entered wrong password: %s" % user
+					reply += "NOK\n"
+					tcp_send(connection, reply)
+					return
+			else:
+				f = open(filename, "w")
+				f.write(password)
+				f.close()
+				os.mkdir(dirname)
+				f=open("bss_" + user + ".txt", "w")
+				f.close()
+				print "New user: %s" % user
+				reply += "NEW\n"
+				tcp_send(connection, reply)
+				return
+
+		tcp_send(connection, reply)
+
+	msg = tcp_receive(connection)
+
+	if msg != None:
+		data_list = msg.split()
+		command = data_list[0]
+		reply = ""
+
+		if command == "DLU": #completo
+			reply += "DLR "
+			if not check_protocol(command, data_list):
+				print "Protocol (syntax) error"
+				reply += "ERR\n"
+			else:
+				print "   Delete user request"
+				path = HOME + "/" + dirname
+				if empty_dir(path):
+					os.rmdir(dirname)
+					os.remove(filename)
+					os.remove("bss_" + user + ".txt")
+					print "User deleted: %s" % user
+					reply += "OK\n"
+				else:
+					print "Failed to delete user: %s" % user
+					reply += "NOK\n"
+
+		elif command == "BCK": #incompleto
+			reply += "BKR "
+			if not check_protocol(command, data_list):
+				print "Protocol (syntax) error"
+				reply += "ERR\n"
+			else:
+				bs_info = get_available_bs()
+				if bs_info == None:
+					print "No BS available"
+					reply += " EOF\n"
+					tcp_send(connection, reply)
+				else:
+					user_dir = data_list[1]
+					n = data_list[2]
+					files = data_list[3:]
+					ip_bs = bs_info[0]
+					port_bs = bs_info[1]
+					bs_address = (ip_bs, int(port_bs))
+					reply += ip_bs + " " + port_bs
+					test_bs = ip_bs + " " + port_bs + "\n"
+					print "   "+command+" "+user+" "+user_dir+" "+ip_bs+" "+port_bs
+					if registered_in_bs(user, test_bs):
+						msg = "LSF " + user + " " + user_dir + "\n"
+						lock.acquire()
+						c = udp_client_init()
+						udp_send(c, msg, bs_address)
+						bs_msg, bs_aux_addr = udp_receive(c)
+						print (bs_msg)
+						udp_terminate(c)
+						lock.release()
+						bs_msg_list = bs_msg.split()
+						response = bs_msg_list[0]
+						n = bs_msg_list[1]
+						bs_files = bs_msg_list[2:]
+						if response == "LFD":
+							reply += " " + n
+							for i in bs_files:
+								reply += " " + i
+							reply += "\n"
+						tcp_send(connection, reply)
+					else:
+						register_in_bs(user, test_bs)
+						msg = "LSU " + user + " " + password + "\n"
+						lock.acquire()
+						c = udp_client_init()
+						udp_send(c, msg, bs_address)
+						bs_msg, bs_aux_addr = udp_receive(c)
+						udp_terminate(c)
+						lock.release()
+						bs_msg_list = bs_msg.split()
+						response = bs_msg_list[0]
+						status = bs_msg_list[1]
+						if response == "LUR":
+							if status == "OK":
+								reply += " " + n
+								for i in files:
+									reply += " " + i
+								reply += "\n"
+							elif status == "NOK":
+								reply += "NOK\n"
+							elif status == "ERR":
+								reply += "ERR\n"
+
+		elif command == "RST": #completo
+			reply += "RSR "
+			if not check_protocol(command, data_list):
+				print "Protocol (syntax) error"
+				reply += "ERR\n"
+			else:
+				rst_dir = data_list[1]
+				bs_info = get_bs_address(user, rst_dir)
+				if bs_info == None:
+					reply += "ERR\n"
+				else:
+					ip_bs = bs_info[0]
+					port_bs = bs_info[1]
+					reply += ip_bs + " " + port_bs
+					print "   Restore " + rst_dir
+
+		elif command == "LSD": #completo
+			reply += "LDR "
+			if not check_protocol(command, data_list):
+				print "Protocol (syntax) error"
+				reply += "ERR\n"
+			else:
+				print "   List request"
+				path = HOME + "/" + dirname
+				dirs = os.listdir(path)
+				n = len(dirs)
+				reply += str(n) 
+				for d in dirs:
+					reply += " " + d
+				reply += "\n"
+
+		elif command == "LSF": #incompleto
+			reply += "LFD"
+			if not check_protocol(command, data_list):
+				print "Protocol (syntax) error"
+				reply += " ERR\n"
+			else:
+				user_dir = data_list[1]
+				msg = "DLB "
+				user_dir = data_list[1]
+				bs_info = get_bs_address(user, user_dir)
+				if bs_info == None:
+					reply += " ERR\n"
+				else:
+					ip_bs = bs_info[0]
+					port_bs = bs_info[1]
+					bs_address = (ip_bs, int(port_bs))
+					lock.acquire()
+					c = udp_client_init()
+					msg += user + " " + user_dir
+					udp_send(c, msg, bs_address)
+					bs_msg, bs_aux_addr = udp_receive(c)
+					udp_terminate(c)
+					lock.release()
+					bs_msg_list = bs_msg.split()
+					response = bs_msg_list[0]
+					if response == "LFD":
+						reply_list = [n]
+						bs_msg_list[2:]
+						reply_list += bs_msg_list
+						for i in reply_list:
+							reply += " " + i
+						reply += "\n"
+
+		elif command == "DEL": #incompleto
+			reply += "DDR "
+			if not check_protocol(command, data_list):
+				print "Protocol (syntax) error"
+				reply += "ERR\n"
+			else:
+				msg = "DLB "
+				user_dir = data_list[1]
+				bs_info = get_bs_address(user, user_dir)
+				if bs_info == None:
+					reply += "ERR\n"
+				else:
+					ip_bs = bs_info[0]
+					port_bs = bs_info[1]
+					bs_address = (ip_bs, int(port_bs))
+					lock.acquire()
+					c = udp_client_init()
+					msg += user + " " + user_dir + "\n"
+					udp_send(c, msg, bs_address)
+					bs_msg, bs_aux_addr = udp_receive(c)
+					udp_terminate(c)
+					lock.release()
+					bs_msg_list = bs_msg.split()
+					response = bs_msg_list[0]
+					status = bs_msg_list[1]
+					if response == "DBR":
+						if status == "OK":
+							reply += "OK\n"
+						elif status == "NOK":
+							reply += "NOK\n"
+						elif status == "ERR":
+							reply += "ERR\n"
+		
+		tcp_send(connection, reply)
+
+	return
+
+def user_tcp(lock):
+	while True:
+		#print "Start tcp"
+		c = tcp_init()
+		#print "Init check tcp"
+		msg = tcp_receive(c)
+		#print "Receive check tcp"
+		handle_user(c, msg, lock)
+		#print "Handle check tcp"
+		tcp_terminate(c)
+		#print "Terminate check tcp"
+	
+
+def bs_udp(lock):
+	#print "Start udp"
+	c = udp_server_init()
+	#print "Init check udp"
+	while True:
+		#print "start while udp"
+		msg, bs_address = udp_receive(c)
+		#print "Receive check udp"
+		handle_bs(c, msg, bs_address)
+		#print "Handle check udp"
+
+def main():
+	try:
+		pid = os.fork()
+	except OSError as e:
+		print ("Error creating child process: %s" % e)
+	if pid == 0:
+		bs_udp(lock)
+	else:
+		user_tcp(lock)
+
+if __name__ == "__main__":
+	main()
